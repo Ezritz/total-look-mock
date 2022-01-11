4cd6a2ac5cc9f463e0ae8c3c684a358cb30c1de8
ezritz <lidia.edith.escoto@gmail.com>
2022-01-11 12:30:05 -0600

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './componentes/Home';
+import Sales from './componentes/Sales';
 import './App.css';
 
 function App() {
   return (
   
     <div className="App">
-      <p>Hello</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Admin"/>
+          <Route path="/"
+            element={<Home />
+            }/>
+          <Route path="/Promociones"
+            element={<Sales />
+            }/>
+          <Route path="Login"/>
+          
+        </Routes>
+      
+      </BrowserRouter>
     </div>
   );
 }
