7dcb0b98e693388b95270e9f1667c9c3ea40801a
mirellgms <mirellgmsmkt@gmail.com>
2022-01-12 11:08:58 -0600

    prueba

---
+++ src/TheCategories.js
@@ -0,0 +1,19 @@
+import Categories from "./Categories"
+import clothes from './clothes.jpg';
+import shoes from './shoes.jpg';
+import handBags from './bags.jpg';
+import kids from './kids.jpg';
+import accesories from './accesories.jpg';
+import others from './others.jpg';
+
+export default function TheCategories(){
+    return(
+        <Categories image = {clothes}/>;
+        <Categories image = {shoes}/>;
+        <Categories image = {handBags}/>;
+        <Categories image = {kids}/>;
+        <Categories image = {accesories}/>;
+        <Categories image = {others}/>;
+        
+    );
+}
--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -1,24 +1,33 @@
 import { useState, useEffect } from 'react';
 import Sales from './Sales';
-import myImage from '../images/search-1.png';
+import Banner from '../Banner';
+import '../css/Home.css';
 
 export default function Inicio () {
-    const [activeMenu, setActiveMenu] = useState('');
-
+    
     return (
         <div>
-            
-            <a>
-                See all promotions >>
-            </a>
             <div>
-                <label>Search products</label>
+                <Banner />
+            </div>
+            <div>
+                <label>Search products </label> 
                 <input
                     type="text"
                     className="search"
                     
                 />
-                <img src={myImage} width="20px"/>
+                <label
+                className="logo-search"
+                >🔎</label>
+            </div>
+            
+            <div>
+                <a>
+                    See all promotions >>
+                </a>
+                
+                
                 
                
             </div>
